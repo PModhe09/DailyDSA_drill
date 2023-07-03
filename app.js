e5198d90c23564db2785a5d6dc446c24ac5a7cde
PrathmeshModhe9 <lastry2001@gmail.com>
2023-07-03 18:47:58 +0530

    Added toggle theme , upcoming contests in side-bar,left to make contest table responsive

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Problem class : represents a particular problem
 let i =1;
+// let problems;
 class Problem{
     constructor(no,platform,problem,link,prio){
          this.no=no;
@@ -24,18 +25,18 @@ class ui{
             const list=document.querySelector('#problem-list');
         const row=document.createElement('tr');
         row.innerHTML=`
-        <td>${Ques.no}</td>
-        <td>${Ques.platform}</td>
-        <td><a href='${Ques.link}'>${Ques.problem}</a></td>
-        <td>${Ques.prio}</td>
-        <td><a href='#' class="btn btn-danger btn-sm delete">X</a></td>
+        <td style="color: white;">${Ques.no}</td>
+        <td style="color: white;">${Ques.platform}</td>
+        <td ><a href='${Ques.link}'>${Ques.problem}</a></td>
+        <td style="color: white;">${Ques.prio}</td>
+        <td style="color: white;"><a href='#' class="btn btn-danger btn-sm delete">X</a></td>
         `;
         
         list.appendChild(row);
     }
 
     static clearFields(){
-        document.querySelector('#platform').value='';
+        // document.querySelector('#platform').value='';
         document.querySelector('#link').value='';
         document.querySelector('#prio').value='';
         document.querySelector('#problem').value='';
@@ -68,8 +69,10 @@ class Store{
         else{
             problems=JSON.parse(localStorage.getItem('problems'));
         }
+       // console.log(problems);
         return problems;
      }
+
      static addProblem(problem){
         const problems=Store.getProblem();
         problems.push(problem);
@@ -96,13 +99,26 @@ document.querySelector('#form').addEventListener('submit',(e)=>{
     // const no=parseInt(document.querySelector('#no').value,10);
     const no=i++;
 
-    const platform=document.querySelector('#platform').value;
+
     const link=document.querySelector('#link').value;
+    function getPlatform(){
+        let tmp=link;
+        if(tmp.search("codeforces")!=-1){
+            return "codeforces"
+        }
+        else if(tmp.search("leetcode")!=-1){
+            return "leetcode"
+        }
+        else if(tmp.search("codechef")!=-1){
+            return "codechef"
+        }
+    }
+    const platform=getPlatform()
     const prio=document.querySelector('#prio').value;
     const problem_n=document.querySelector('#problem').value;
   
     
-    if(no==='' || platform==='' || link==='' || problem_n===''){
+    if(no==='' || platform==='' || link==='' || problem_n==='' || prio===''){
         ui.showAlert('Some inputs are empty','danger')
     }
     else{
@@ -125,3 +141,122 @@ document.querySelector('#problem-list').addEventListener('click',(e)=>{
      Store.removeProblem(e.target.parentElement.previousElementSibling.textContent);
       ui.showAlert('Problem Solved','success')
 });
+
+let constestList;
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100vw";
+    // Fetch the data from the API
+fetch("https://kontests.net/api/v1/codeforces")
+.then((response) => response.json())
+.then((data) => {
+  // Create the table header
+  const tableHeader = "<tr><th>Name</th><th>Start Time</th><th>Duration</th></tr>";
+
+  // Create the table body
+  let tableBody = "";
+  data.forEach((contest) => {
+    const name = contest.name;
+    const url = contest.url;
+    const startTime = contest.start_time || "-";
+    const duration = contest.duration || "-";
+    const status = contest.status;
+    console.log(typeof(startTime))
+    const tableRow = `<tr><td style="font-size:2px;"><a href=${url}>${name}</a></td><td>${startTime}</td><td>${duration}</td></tr>`;
+    tableBody += tableRow;
+  });
+
+  // Create the complete table
+  const table = `<table>${tableHeader}${tableBody}</table>`;
+
+  // Append the table to an element in the HTML document
+  document.getElementById("table-container-cf").innerHTML = table;
+})
+.catch((error) => {
+  console.error("Error:", error);
+});
+
+
+fetch("https://kontests.net/api/v1/leet_code")
+.then((response) => response.json())
+.then((data) => {
+  // Create the table header
+  const tableHeader = "<tr><th>Name</th><th>Start Time</th><th>Duration</th></tr>";
+
+  // Create the table body
+  let tableBody = "";
+  data.forEach((contest) => {
+    const name = contest.name;
+    const url = contest.url;
+    const startTime = contest.start_time || "-";
+    const duration = contest.duration || "-";
+    const status = contest.status;
+    console.log(typeof(startTime))
+    const tableRow = `<tr><td style="font-size:2px;"><a href=${url}>${name}</a></td><td>${startTime}</td><td>${duration}</td></tr>`;
+    tableBody += tableRow;
+  });
+
+  // Create the complete table
+  const table = `<table>${tableHeader}${tableBody}</table>`;
+
+  // Append the table to an element in the HTML document
+  document.getElementById("table-container-lc").innerHTML = table;
+})
+.catch((error) => {
+  console.error("Error:", error);
+});
+
+fetch("https://kontests.net/api/v1/code_chef")
+.then((response) => response.json())
+.then((data) => {
+  // Create the table header
+  const tableHeader = "<tr><th>Name</th><th>Start Time</th><th>Duration</th></tr>";
+
+  // Create the table body
+  let tableBody = "";
+  data.forEach((contest) => {
+    const name = contest.name;
+    const url = contest.url;
+    const startTime = contest.start_time || "-";
+    const duration = contest.duration || "-";
+    const status = contest.status;
+    console.log(typeof(startTime))
+    document.getElementById("tr").style.color = "blue";
+    const tableRow = `<tr style="border: 4px; border-color: #4cd137;><td><a href=${url}>${name}</a></td><td>${startTime}</td><td>${duration}</td></tr>`;
+    
+    tableBody += tableRow;
+  });
+
+  // Create the complete table
+  const table = `<table>${tableHeader}${tableBody}</table>`;
+
+  // Append the table to an element in the HTML document
+  document.getElementById("table-container-cc").innerHTML = table;
+})
+.catch((error) => {
+  console.error("Error:", error);
+});
+
+  
+
+  }
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+let dark=false;
+function darkMode(){
+   if(dark==false){
+    document.body.style.backgroundColor="#706fd3";
+    dark=true;
+   }
+   else{
+    document.body.style.backgroundColor="#2c2c54";
+    dark=false
+   }
+}
+
+// function openBot(){
+//     console.log('bot')
+//     document.getElementById("bot").style.width = "100vw";
+// }
